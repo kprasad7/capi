a28c62bf1e2565de0438fe3e4487dc3e973367b6
kprasad7 <kanuridurgaprasad049@gmail.com>
2021-03-13 09:45:08 +0530

    uploading

---
+++ restapp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+
+from restapp.models import File
+
+
+admin.site.register(File)
+
---
+++ studentbio/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+
+from studentbio.models import studentdata
+admin.site.register(studentdata)
+
